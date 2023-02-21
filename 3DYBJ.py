13895a47287dacbd76a199ecd8bd22acb2a410d5
scott-conn <71698037+scott-conn@users.noreply.github.com>
2023-02-21 10:16:28 -0800

    Tidied up and added comments

--- 3DYBJ.py
+++ 3DYBJ.py
@@ -32,25 +32,33 @@ if MPI.COMM_WORLD.size > (Nx // 2):
 else:
     mesh = None
     
-def spin_up_QG(Ls,ns,init_file,event_file,Nt,time_step,f_0)
+def spin_up_QG(Ls,ns,p,init_file,strat_file)
     Lx, Ly, Lz = Ls                                                                     #length of each dimension
     nx, ny, nz = ns                                                                     #number of grid cells in each dimension
     Tx, Ty, Tz = (Lx/nx,Ly/ny,Lz/nz)
     x_basis = de.Fourier('x', nx, interval=(-Lx/2, Lx/2), dealias=3/2)                  #create x-dimension
     y_basis = de.Fourier('y', ny, interval=(-Ly/2, Ly/2), dealias=3/2)                  #create y-dimension 
     z_basis = de.Chebyshev('z', nz, interval=(-Lz, 0), dealias=3/2)                     #create z-dimension
-
-    #Stratification parameter
+    
+    #parameters
+    f_0 = p[1]
+    T = 2*np.pi/f_0
+    Nt = p[2]
+    dt = p[3]
+    nuq = p[4]
+    kappa = p[5]
     N2 = domain.new_field()
     N2.meta['x','y']['constant'] = True
-    hf = h5py.File(event_file, 'r')                                                     #open file containing simulation data
+    hf = h5py.File(strat_file, 'r')                                                    
     N2_data = hf.get('N2')
     N2['g'] = N2_data
     close(hf)
     
+    #create IVP with the streamfunction (psi) and vertical velocity (w) as parameters
     problem = de.IVP(domain, variables=['psi','w'],ncc_cutoff=1e-8)
     problem.meta[:]['z']['dirichlet'] = True
 
+    #define differential operators, QGPV and buoyancy
     problem.substitutions['L(a)'] = "d(a,x=2) + d(a,y=2)"
     problem.substitutions['HD(a)'] = "L(L(L(L(a))))"   
     problem.substitutions["J(f,g)"] = "dx(f)*dy(g)-dy(f)*dx(g)"
@@ -59,6 +67,8 @@ def spin_up_QG(Ls,ns,init_file,event_file,Nt,time_step,f_0)
     
     problem.parameters['f'] = f_0
     problem.parameters['N2'] = N2
+    probem.parameters['nu'] = nuq
+    problem.parameters['kappa'] = kappa
     
     problem.add_equation("dt(q) + nu*HD(q)  = -J(psi,q)")
     problem.add_equation("dt(b) + N2*w  + kappa*HD(b) = -J(psi,b)")
@@ -71,12 +81,14 @@ def spin_up_QG(Ls,ns,init_file,event_file,Nt,time_step,f_0)
     for g in [psi_init, b_init, Q_init]:
         g.set_scales(domain.dealias)
 
-    slices = domain.dist.grid_layout.slices(scales=1)                                           #take right part of psi array when in parallel
+    #slices ensures that in parallel the right part of the input file is given to the right node
+    slices = domain.dist.grid_layout.slices(scales=1)                                           
     hf = h5py.File(init_file, 'r')  
-    psi_data = hf.get('psi')                                                                   #extract psi data
+    psi_data = hf.get('psi')                                                                
     psi_init['g'] = psi_data[:,:,:][slices]
     hf.close()
     
+    #Invert the initial psi for w
     # Need to solve for W_init using dt(P) --> Pt_init as a slack variable.
     init_problem = de.LBVP(domain, variables=['Pt','w'])
     init_problem.meta[:]['z']['dirichlet'] = True
@@ -103,10 +115,12 @@ def spin_up_QG(Ls,ns,init_file,event_file,Nt,time_step,f_0)
     init_solver.state['w'].set_scales(domain.dealias)
     w['g'] = init_solver.state['w']['g']
                
-    solver.stop_iteration = Nt
+    #set spin-up time
+    solver.stop_iteration = Nt*T
     solver.stop_sim_time  = np.inf
     solver.stop_wall_time = np.inf           
                
+    #run simulation
     logger.info('Starting loop')
     start_time = time.time()
     dt = time_step
@@ -118,7 +132,7 @@ def spin_up_QG(Ls,ns,init_file,event_file,Nt,time_step,f_0)
                
     return solver     
                
-def run_sim(Ls,ns,f_0,Hm,solver_QG,event_file,Nt,Nw,time_step):
+def run_sim(Ls,ns,p,solver_QG,strat_file,wind_file):
     
     Lx, Ly, Lz = Ls                                                                     #length of each dimension
     nx, ny, nz = ns                                                                     #number of grid cells in each dimension
@@ -130,27 +144,28 @@ def run_sim(Ls,ns,f_0,Hm,solver_QG,event_file,Nt,Nw,time_step):
     #create problem with M, Mz, Mzz, psi and w as the variables
     domain = de.Domain([x_basis, y_basis, z_basis], grid_dtype=np.complex128)
     problem = de.IVP(domain, variables=['M','Mz','Mzz','psi','w'],max_ncc_terms=10)
-
-    #problem parameters
-    z = domain.grid(2)
-    T = 2*np.pi/f_0                                                                     #inertial period (s)
-    a = 2                                                                               #parameter governing sharpness of forcing profile
-    M0 = 0+0j                                                                           #start simulation with no NIWs, boundary term on is zero at top
-    nuM = 3e6                                                                           #hyperdiffusivity parameter for NIWs (m^4/s)
-    nuq = 3e6                                                                           #hyperdiffusivity parameter for q
-    kappa = 3e6                                                                         #hyperdiffusivity parameter for b
-    dt = time_step*T                                                                    #timestep (s)
-    psi_update = 24*60*60//dt                                                           #number of timesteps per day (number of timesteps after which update psi)
     
-    hf = h5py.File(event_file, 'r')                                                     #open file containing simulation data
-    N2_data = hf.get('N2')                                                              #extract stratification profile
-    
-    #Dedalus procedure to add parameter varying in z
-    ncc = domain.new_field(name='c')                                                    
-    ncc['g'] = N2_data
-    ncc.meta['x', 'y']['constant'] = True
-    
-    wind_data = hf.get('F')                                                             #extract wind data
+    #parameters
+    f_0 = p[1]
+    T = 2*np.pi/f_0
+    Nt = p[2]
+    dt = p[3]
+    nuq = p[4]
+    kappa = p[5]
+    nuM = p[6]
+    a = p[7]
+    Hm = p[8]
+    Nw = p[9]
+    M0 = 0+0j
+    N2 = domain.new_field()
+    N2.meta['x','y']['constant'] = True
+    hf = h5py.File(strat_file, 'r')                                                    
+    N2_data = hf.get('N2')
+    N2['g'] = N2_data
+    close(hf)
+    hf = h5py.File(wind_file, 'r')
+    wind_data = hf.get('F')[:]                                                          #extract wind data
+    close(hf)
 
     #parameters
     problem.parameters['f'] = f_0
@@ -177,10 +192,8 @@ def run_sim(Ls,ns,f_0,Hm,solver_QG,event_file,Nt,Nw,time_step):
     problem.substitutions['q'] = "L(psi) + dz(f**2*dz(psi)/N2) + 1j*J(conj(Mz),Mz)/2f + L(mag2(Mz))/4/f"
     problem.substitutions['b'] = "f*dz(psi)"
     
-
     #equations and b.c.'s
     problem.add_equation("dt(Mzz) + 1j*N2*L(M)/2/f + nu*HD(Mzz) = -J(psi,Mzz) -1j*L(psi)*Mzz/2 + W*forc_zz") 
-    problem.add_equation("dt(psi) = 0")
     problem.add_equation("Mzz - dz(Mz) = 0")
     problem.add_equation("Mz - dz(M) = 0")
     problem.add_equation("dt(q) + nu*HD(q)  = -J(psi,q)")
@@ -212,36 +225,13 @@ def run_sim(Ls,ns,f_0,Hm,solver_QG,event_file,Nt,Nw,time_step):
     w['g'] = solver_QG.state['w']['g']
     psi['g'] = solver_QG.state['psi']['g']
     
-    #set up output files
-    #state variables
+    #set up output file
     state_file = "state" #output file name
     analysis = solver.evaluator.add_file_handler(state_file, iter=Nw)
     analysis.add_system(solver.state, layout='g')
     analysis.add_task('W',name='wind')
-    analysis.add_task('nabla2(psi)',name='zeta')
-
-    #energy budget terms
-    energy_file = "energy"
-    analysis_E = solver.evaluator.add_file_handler(energy_file,iter=Nw)
-    analysis_E.add_task('1j*N2*(M*dx(conj(M))-conj(M)*dx(M))/4/f**2',name='Fx')
-    analysis_E.add_task('1j*N2*(M*dy(conj(M))-conj(M)*dy(M))/4/f**2',name='Fy')
-    analysis_E.add_task('1j*N2*(M*nabla2(conj(M))-conj(M)*nabla2(M))/4/f**2',name='divF')
-    analysis_E.add_task('N2*M/f**2',name='Az')
-    analysis_E.add_task('J(psi,mag2(Mz))/2/f',name='advection_A')
-    analysis_E.add_task('(conj(Mz)*(W*forc_z)+Mz*conj(W*forc_z))/2/f',name='gamma_A')
-    analysis_E.add_task('nu*real(conj(Mz)*nabla4(Mz))/f',name='d_A')
-    analysis_E.add_task('N2*(dx(M)*dx(conj(M))+dy(M)*dy(conj(M)))/4/f**2',name='PE')
-
-    #domain integrated budget terms
-    budget_file = "budget"
-    analysis_b = solver.evaluator.add_file_handler(budget_file,iter=Nw)
-    analysis_b.add_task('integ(mag2(Mz)/2/f)',name='A')
-    analysis_b.add_task('integ(conj(Mz)*(W*forc_z)+Mz*conj(W*forc_z))/2/f',name='Gamma_A')
-    analysis_b.add_task('integ(nu*real(conj(Mz)*nabla4(Mz))/f)',name='D_A')
-    analysis_b.add_task('integ(N2*(dx(M)*dx(conj(M))+dy(M)*dy(conj(M)))/4/f**2)',name='P')
-    analysis_b.add_task('integ(1j*N2*nabla2(psi)*(-M*nabla2(conj(M))+conj(M)*nabla2(M))/8/f**2)',name='Gamma_P_ref')
-    #analysis_b.add_task('integ(N2*real(dx(M)*J(dx(psi),conj(M)) + dy(M)*J(dy(psi),conj(M)))/2/f**2)',name='Gamma_P_adv')
-    analysis_b.add_task('integ(nu*N2*real(nabla2(conj(M))*nabla4(M))/f**2)',name='D_P')
+    analysis.add_task('q',name='PV')
+    analysis.add_task('b',name='buoyancy')
 
     logger.info('Starting loop')
     start_time = time.time()
@@ -251,15 +241,6 @@ def run_sim(Ls,ns,f_0,Hm,solver_QG,event_file,Nt,Nw,time_step):
         M0 = M0 + dt*wind_data[solver.iteration-1]                                  #solve ODE for top b.c.
         problem.namespace['M_0'].value = M0                                         #update top b.c.
         problem.namespace['W'].value = wind_data[solver.iteration]                  #update wind forcing
-        
-        #check if time to update psi and do so if necessary
-        if solver.iteration % psi_update == 0:                                      
-            index = np.int64(solver.iteration/psi_update)
-            psi.set_scales(1)
-            psi_data = psi_data1[:,:,:,index]
-            slices = domain.dist.grid_layout.slices(scales=1)
-            psi['g'] = psi_data[slices]
-            
         if (solver.iteration-1) % 1000 == 0:
             for field in solver.state.fields: field.require_grid_space()
             logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, dt))
